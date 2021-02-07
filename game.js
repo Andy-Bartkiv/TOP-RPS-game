480e279a2fea8d5117d9a59df60a0edf268a5715
andy <andy.bartkiv@gmail.com>
2021-02-08 02:09:21 +0300

    simple UI finished

--- game.js
+++ game.js
@@ -1,51 +1,66 @@
 function compPlay() {
-    let res ='';
-    switch(Math.floor(Math.random() * 3) + 1) {
-        case 1: res = 'Rock'; break;
-        case 2: res = 'Paper'; break;
-        case 3: res = 'Scissors'; break;
-        default: res = 'NOT GOOD at all'
-    }
-    return res;
+    return {
+        1: "Rock",
+        2: "Paper",
+        3: "Scissors"
+    }[Math.floor(Math.random() * 3) + 1]
 }
 
-function oneRound(playerSelect, compSelect) {
-    let res = '';
-    switch(playerSelect.toLowerCase()) {
-        case 'rock': 
-            if (compSelect === 'Rock') {res = 'It \'s a draw!'
-            } else if (compSelect === 'Paper') {res = 'You loose!'; score[1]+=1;
-            } else if (compSelect === 'Scissors') {res = 'You WIN!'; score[0]+=1;}
-            break;
-        case 'paper': 
-            if (compSelect === 'Rock') {res = 'You WIN!'; score[0]+=1;
-            } else if (compSelect === 'Paper') res = 'It \'s a draw!';
-            else if (compSelect === 'Scissors') {res = 'You loose!'; score[1]+=1;}
-            break;
-        case 'scissors': 
-            if (compSelect === 'Rock') {res = 'You loose!'; score[1]+=1;
-            } else if (compSelect === 'Paper') {res = 'You WIN!'; score[0]+=1;
-            } else if (compSelect === 'Scissors') res = 'It \'s a draw!';
-            break;
-    }
-    return `YOU: ${playerSelect}, COMP: ${compSelect} ... ` + res;
+function whoWins(playerSelect, compSelect) {
+    const b = ["Rock", "Paper", "Scissors"];
+    return [
+        ["D", "L", "W"],
+        ["W", "D", "L"], 
+        ["L", "W", "D"]]
+        [b.indexOf(playerSelect)][b.indexOf(compSelect)];
 }
 
-function game() {
-    score = [0,0];
-    let winner;
-    let humanPlay = '';
-    for (i = 1; i < 6; i++) {
-        humanPlay = prompt(`Rock, Paper or Scissors? ${score}`)
-        console.log('Round ' + i + ' -> ' + oneRound(humanPlay,compPlay()) + 
-        ' ... Total Score is ' + score[0] + " : " + score[1]);
-    } 
-    winner = score[0] - score[1];
-    winner = (winner > 0) ? "Human Wins" : 
-        (winner < 0) ? "Comp Wins" : "A TIE";
-    console.log('- - - THE END - - - ' + winner);
-    
+function restartGame() {
+    console.log('RESTART');
+    const del = [...document.querySelectorAll('.content')];
+    del.forEach(e => results.removeChild(e));
+    buttons.forEach(but => but.disabled = false);
+    score = [0,0,0];
 };
 
-let score = [];
-document.getElementById("Run").onclick = game;
+function playRound(humSel, compSel) {
+    score[0]++;
+    switch(whoWins(humSel,compSel)) {
+        case 'D':             break;
+        case 'W': score[1]++; break;
+        case 'L': score[2]++; break;
+        default: score[0] = 'ERROR';
+    }
+    
+    const content = document.createElement('div');
+    content.classList.add('content');
+    content.textContent = `Round ${score[0]} ->  ${humSel} ${score[1]} : ${score[2]} ${compSel} `;
+    results.appendChild(content);
+
+    update(5);
+}
+
+function update(maxScore) {
+    let winner = false;
+    if (score[1] >= maxScore) winner = "H"
+    else if (score[2] >= maxScore) winner = "C";
+    if (winner) {
+        buttons.forEach(but => but.disabled = true);
+        alert('THE END \n' + ((winner === "H") ? "!You Win!" : "You Loose"));
+    }
+}
+
+let score = [0,0,0];
+
+const buttons = [...document.querySelectorAll('.play-buttons')]
+
+const results = document.querySelector('#round-res');
+
+const butRestart = document.querySelector('#but-restart')
+butRestart.addEventListener('click', restartGame);
+
+buttons.forEach(but => { 
+    but.addEventListener('click', () => {
+        playRound(but.id, compPlay())
+    })
+});
